352bca84c0f49236ae636bb7bbeb5245c93c0391
AMAN GUPTA <amangupta1995.ag@gmail.com>
2023-01-05 11:40:36 +0530

    Update date.js

--- ReactRedux- Hotel Researvation System [ITU, Denmark]/js/date.js
+++ ReactRedux- Hotel Researvation System [ITU, Denmark]/js/date.js
@@ -3,7 +3,7 @@
  * other code. Doens't overwrite existing methods.  
  *  
  * Adds dayNames, abbrDayNames, monthNames and abbrMonthNames static properties and isLeapYear,     
- * isWeekend, isWeekDay, getDaysInMonth, getDayName, getMonthName, getDayOfYear, getWeekOfYear,  
+ * isWeekend, isWeekDay, getDaysInMonth, getDayName, getMonthName, getDayOfYear, getWeekOfYear,   
  * setDayOfYear, addYears, addMonths, addDays, addHours, addMinutes, addSeconds methods 
  * 
  * Copyright (c) 2006 Jörn Zaefferer and Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)

4d6da31cfbe1e9e4330f3e8872767c2d981dfff1
AMAN GUPTA <amangupta1995.ag@gmail.com>
2023-01-05 11:40:02 +0530

    Update date.js

--- ReactRedux- Hotel Researvation System [ITU, Denmark]/js/date.js
+++ ReactRedux- Hotel Researvation System [ITU, Denmark]/js/date.js
@@ -1,7 +1,7 @@
 /*
  * Date prototype extensions. Doesn't depend on any 
  * other code. Doens't overwrite existing methods. 
- * 
+ *  
  * Adds dayNames, abbrDayNames, monthNames and abbrMonthNames static properties and isLeapYear,   
  * isWeekend, isWeekDay, getDaysInMonth, getDayName, getMonthName, getDayOfYear, getWeekOfYear, 
  * setDayOfYear, addYears, addMonths, addDays, addHours, addMinutes, addSeconds methods 
